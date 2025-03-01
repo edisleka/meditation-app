1bc0b5db14f99a4538fa1329f9c70abddf4dd8f3
Edis Leka <edisleka@yahoo.gr>
2025-03-02 01:21:13 +0200

    nativewind setup

---
+++ src/app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from 'react-native'
+
+export default function HomeScreen() {
+  return (
+    <View>
+      <Text className='font-bold'>Hello, world!</Text>
+    </View>
+  )
+}
